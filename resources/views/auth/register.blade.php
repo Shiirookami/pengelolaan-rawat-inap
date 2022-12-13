a3b6b190fc42b0675bdf58369dd57f7e3ad1965a
resarisyan <resarisyan77@gmail.com>
2022-12-13 11:02:51 +0700

    add login and register features

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.auth_master')
+
+@section('title')
+Register 
+@endsection
+
+@section('content')
+<div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card-body p-0">
+      <!-- Nested Row within Card Body -->
+      <div class="row">
+        <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+        <div class="col-lg-7">
+          <div class="p-5">
+            <div class="text-center">
+              <h1 class="h4 text-gray-900 mb-4">{{ __('Register') }}</h1>
+            </div>
+            <form method="POST" class="user" action="{{ route('register') }}"> @csrf <div class="form-group">
+                <input type="text" class="form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" id="exampleName" placeholder="{{ __('Name') }}" required autocomplete="name" autofocus> @error('name') <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span> @enderror
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" id="exampleInputEmail" placeholder="{{ __('Email Address') }}" required autocomplete="email"> @error('email') <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span> @enderror
+              </div>
+              <div class="form-group row">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                  <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" id="exampleInputPassword" placeholder="{{ __('Password') }}" required autocomplete="new-password"> @error('password') <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                  </span> @enderror
+                </div>
+                <div class="col-sm-6">
+                  <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" name="password_confirmation" placeholder="{{ __('Confirm Password') }}" required autocomplete="new-password">
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary btn-user btn-block">{{ __('Register') }}</button>
+            </form>
+            <hr>
+            <div class="text-center">
+              <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>  
+@endsection
