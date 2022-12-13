0f08a4b2d6c23d829a4a9ad0c876ee947e9815a9
resarisyan <resarisyan77@gmail.com>
2022-12-13 11:53:56 +0700

    add role migration

--- database/migrations/2013_12_13_044544_create_roles_table.php
+++ database/migrations/2013_12_13_044544_create_roles_table.php
@@ -13,18 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('status', [0, 1])->default(0);
-            $table->unsignedBigInteger('roles_id');
-            $table->foreign('roles_id')->references('id')->on('roles');
-            $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -35,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
