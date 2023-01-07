5f00e43eb62aad116c7ac323011e76828d14c121
Shiirookami <rzkir818@gmail.com>
2023-01-08 06:20:19 +0700

    add data dashboard

--- app/Http/Controllers/Petugas/DashboardController.php
+++ app/Http/Controllers/Petugas/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Petugas;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dokter;
 use Illuminate\Http\Request;
+use App\Models\Kamar;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('petugas.index');
+        $data['kamars'] = Kamar::all();
+        $data['dokters'] = Dokter::all();
+        return view('petugas.index')->with($data);
     }
 }
--- resources/views/petugas/index.blade.php
+++ resources/views/petugas/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.dashboard_master')
+@section('content')
 @section('title')
     Halaman Dashboard
 @endsection
-@section('content')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
@@ -21,7 +21,9 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Kamar Kelas VIP</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">0 Orang</div>
+                                @foreach ($kamars as $item)
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$item->kelas}}</div>
+                                @endforeach
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -37,7 +39,9 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Kamar Kelas I</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">0 Orang</div>
+                            @foreach ($kamars as $item)
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$item->kelas}}</div>
+                            @endforeach
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -53,7 +57,9 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Kamar Kelas II</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">0 Orang</div>
+                            @foreach ($kamars as $item)
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$item->kelas}}</div>
+                            @endforeach
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -69,7 +75,9 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Kamar Kelas III</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">0 Orang</div>
+                            @foreach ($kamars as $item)
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$item->kelas}}</div>
+                            @endforeach
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -81,78 +89,33 @@
     </div>
 
     <!-- Content Row -->
-
-    <div class="row">
-
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="myAreaChart"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-pie pt-4 pb-2">
-                        <canvas id="myPieChart"></canvas>
-                    </div>
-                    <div class="mt-4 text-center small">
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Direct
-                        </span>
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-success"></i> Social
-                        </span>
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-info"></i> Referral
-                        </span>
-                    </div>
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"> Dokter </h5>
+            <!-- Table with hoverable rows -->
+            <table class="table table-hover" id="mytable">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Dokter</th>
+                        <th scope="col">No Indetitas</th>
+                        <th scope="col">Spesialis</th>
+                        {{-- <th scope="col">Tanggal</th> --}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($dokters as $item => $dokter)
+                        <tr>
+                            <th scope="row">{{ $item + 1 }}</th>
+                            <td>{{ $dokter->nama_dokter }}</td>
+                            <td>{{ $dokter->no_identitas }}</td>
+                            <td>{{ $dokter->spesialis }}</td>
+                            {{-- <td>{{ $dokter->jadwal_dokter }}</td> --}}
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <!-- End Table with hoverable rows -->
         </div>
     </div>
 @endsection
